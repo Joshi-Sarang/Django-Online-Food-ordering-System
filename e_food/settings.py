4701b5011d51767784ece75b7f1e2d514e2f5126
Joshi-Sarang <69730480+Joshi-Sarang@users.noreply.github.com>
2021-04-25 22:06:18 +0530

    Update settings.py

--- e_food/settings.py
+++ e_food/settings.py
@@ -126,14 +126,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/images/'
+
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
